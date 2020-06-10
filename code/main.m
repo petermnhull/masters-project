bc84b4bdf6011225cec9284f54d7f0563dd7860b
petermnhull <56369394+petermnhull@users.noreply.github.com>
2020-06-10 11:55:47 +0100

    Minor

--- code/main.m
+++ code/main.m
@@ -5,27 +5,26 @@ N_swimmers = 2;
 % ----- Parameters for swimmers -----
 % Initial positions of each swimmer
 x = [0, 20];
-y = [0, 0];
-alpha = [0, 0];
+y = [0, 0];                  
+alpha = [0, 0];                % Orientation
 
 % Basic parameters
-lambda_s = [15, 25];
-k_s = [1.5, 1.5];
-beta_A = [1, 1];
-zeta = [0.5, 1];
-gamma = [5, 1];
+lambda_s = [10, 10];           % Amplitude Ratio
+k_s = [1.5, 3];                % Wavenumber
+beta_A = [1, 1];               % Multiplier on Amplitude
+zeta = [1, 1];                 % Body / Tail Ratio (1 = no tail, 0 = all tail)
+gamma = [5, 10];               % Phase
 phi = [0, 0];
 
-% ----- Motion type (1 = symmetric, 2 = asymmetric I, 3 = asymmetric II, 4 = peristalsis) -----
-locomotion_type = [4, 1];
+% ----- Motion type (1 = symmetric, 2 = peristalsis, 3 = asymmetric I, 4 = asymmetric II) -----
+locomotion_type = [1, 4];
 
 % ----- Passive links -----
 passive_links = true;
 
-
 % ----- Plotting and Saving ------
 % For plots
-limit = 0.5;    % 0.5 for individuals, and 1 for multiple
+limit = 0.5;    % Recommended: 0.5 for individuals, and 1 for multiple
 filename = strcat(datestr(now, 'yyyymmdd-HHMMSS'), '.txt');  
 save_to_file = false;
 
